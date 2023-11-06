e3559d79fefbeba9a51e22cff69ed8af435ba00e
makarovas <makarofalex@gmail.com>
2023-11-06 16:06:09 +0300

    modify config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.19",
   networks: {
     hardhat: {
       chainId: 1337
